905493f20af1badc8f9e3a4763b4d993c02cc6ee
Ugo Matrangolo <umatrangolo@gilt.com>
2013-08-07 00:23:53 +0100

    Retrieving ApiKey from (i) sys properties and (ii) from giltapi.properties file

--- src/main/scala/com/umatrangolo/giltapi/utils/ApiKey.scala
+++ src/main/scala/com/umatrangolo/giltapi/utils/ApiKey.scala
@@ -1,7 +1,22 @@
 package com.umatrangolo.giltapi.utils
 
+import scala.util.control.Exception._
+import java.util.Properties
+import java.io.IOException
+
+/**
+ * Tries to fetch the ApiKey first from the sys properties and then
+ * from the `giltapi.properties` file in the classpath.
+ */
 object ApiKey {
-  // TODO fetch the key from (i) System.getProperty or (ii) giltapi.properties in the classpath or None
-  //      if both failed
-  lazy val value: Option[String] = Some("b744156e9efa7ef31abf78d030d99ba3")
+  val ApiKeyProperty = "giltapi.client.apikey"
+
+  lazy val value: Option[String] = Option(System.getProperty(ApiKeyProperty)).orElse {
+    val props = new Properties()
+    val is = catching(classOf[IOException]) opt this.getClass.getClassLoader.getResourceAsStream("giltapi.properties")
+    is.map { s =>
+      props.load(s)
+      props.getProperty(ApiKeyProperty)
+    }
+  }
 }
